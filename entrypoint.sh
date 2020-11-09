36cea7e0a2afbeeaad96b5771340f07c7c0f49fc
lxz <justforlxz@gmail.com>
2020-11-09 14:55:23 +0800

    update

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,4 +7,12 @@ EOF
 wget -qO - https://packages.justforlxz.com/deepingit.asc | pacman-key --add -
 pacman-key --finger DCAF15B4605D5BEB
 pacman-key --lsign-key DCAF15B4605D5BEB
-pacman -Syy
+pacman -Sy --noconfirm git clang pkgconfig
+
+git clone -b packages/${repo}-git https://github.com/justforlxz/deepin-git-repo
+pacman -Sy --noconfirm $(cat deepin-git-repo/PKGBUILD | grep -i "makedepends" | grep -Po '(?<=\().*(?=\))' | sed s#\'##g)
+source deepin-git-repo/PKGBUILD
+rm -rf deepin-git-repo
+export pkgname=${GITHUB_WORKSPACE}
+cd ${GITHUB_WORKSPACE}
+build
