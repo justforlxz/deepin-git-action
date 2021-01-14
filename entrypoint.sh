803da77f8392d5c4a8c1e5fdb1912a78a91ec471
justforlxz <justforlxz@gmail.com>
2021-01-14 18:00:51 +0800

    update

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,12 +4,11 @@ repo=$1
 
 cat >> /etc/pacman.conf << EOF
 [deepingit]
-Server = https://packages.justforlxz.com/
+Server = https://repo.archlinuxcn.org/$arch
 EOF
-wget -qO - https://packages.justforlxz.com/deepingit.asc | pacman-key --add -
-pacman-key --finger DCAF15B4605D5BEB
-pacman-key --lsign-key DCAF15B4605D5BEB
-pacman -Sy --noconfirm git clang pkgconfig
+
+pacman -Syu --noconfirm archlinuxcn-keyring
+pacman -Syy
 
 git clone -b packages/${repo}-git https://github.com/justforlxz/deepin-git-repo
 pacman -Sy --noconfirm base-devel $(cat deepin-git-repo/PKGBUILD | grep -i "makedepends" | grep -Po '(?<=\().*(?=\))' | sed s#\'##g)
