0a4ca5d84e273c8d41b70bcc5a75f7f2d0b5da38
justforlxz <justforlxz@gmail.com>
2021-01-15 10:16:11 +0800

    add telegram bot

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 repo=$1
+BUILD_URL="${GITHUB_SERVER_URL}/${GITHUB_REPOSITORY}/actions/runs/${GITHUB_RUN_ID}"
+
+function send_message() {
+    curl https://api.telegram.org/bot${TELEGRAM_BOT_KEY}/sendMessage?chat_id=@deepingitnews&text=$repo+\ +$BUILD_URL
+}
 
 cat >> /etc/pacman.conf << EOF
 
@@ -17,4 +22,4 @@ source deepin-git-repo/PKGBUILD
 rm -rf deepin-git-repo
 export pkgname=${GITHUB_WORKSPACE}
 cd ${GITHUB_WORKSPACE}
-build
+build || send_message
